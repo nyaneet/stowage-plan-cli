2c7628bcf29937521d7d54603d030e3ae0a43726
nyaneet <nyaneet@gmail.com>
2024-09-25 20:42:44 +0300

    feat: add stowage plan for testing

--- bin/stowage_plan.dart
+++ bin/stowage_plan.dart
@@ -1,5 +1,16 @@
-import 'package:stowage_plan/main.dart' as stowage_plan;
-
+import 'package:stowage_plan/main.dart';
+import 'package:stowage_plan/models/container/container_1cc.dart';
+import 'package:stowage_plan/models/stowage/stowage_collection.dart';
 void main(List<String> arguments) {
-  print('Hello world: ${stowage_plan.calculate()}!');
+  final plan = StowageCollection.fromStowageSlotList(
+    arkSlotOrigins.sublist(0, 12),
+  );
+  print(plan);
+  plan.putContainerAt(
+    Container1CC(),
+    bay: 1,
+    row: 1,
+    tier: 2,
+  );
+  print(plan);
 }
