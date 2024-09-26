66778ddfa7c1f4b482809142733f070f00df73c8
nyaneet <nyaneet@gmail.com>
2024-09-27 00:53:28 +0300

    feat: add pretty printing of bay plan for manual testing

--- bin/stowage_plan.dart
+++ bin/stowage_plan.dart
@@ -1,39 +1,39 @@
 import 'package:stowage_plan/main.dart';
 import 'package:stowage_plan/models/container/container_1cc.dart';
 import 'package:stowage_plan/models/stowage/stowage_collection.dart';
+import 'package:stowage_plan/models/stowage/pretty_print_plan.dart';
 void main(List<String> arguments) {
   final plan = StowageCollection.fromSlotList(
-    arkSlotOrigins.sublist(0, 4),
+    arkSlotOrigins.sublist(0, 12),
   );
-  print(plan);
-  print('add 2');
+  plan.printAll();
   plan.addContainer(
     Container1CC(id: 1),
     bay: 1,
     row: 1,
     tier: 2,
   );
-  print(plan);
-  print('add 4');
   plan.addContainer(
-    Container1CC(id: 2),
+    Container1CC(id: 1),
     bay: 1,
-    row: 1,
-    tier: 4,
-  );
-  print(plan);
-  print('remove 2');
-  plan.removeContainer(
-    bay: 1,
-    row: 1,
+    row: 4,
     tier: 2,
   );
-  print(plan);
-  print('remove 4');
-  plan.removeContainer(
-    bay: 1,
-    row: 1,
-    tier: 4,
-  );
-  print(plan);
+  plan.printAll();
+  // plan.addContainer(
+  //   Container1CC(id: 2),
+  //   bay: 1,
+  //   row: 1,
+  //   tier: 4,
+  // );
+  // plan.removeContainer(
+  //   bay: 1,
+  //   row: 1,
+  //   tier: 2,
+  // );
+  // plan.removeContainer(
+  //   bay: 1,
+  //   row: 1,
+  //   tier: 4,
+  // );
 }
---
+++ lib/models/stowage/pretty_print_plan.dart
@@ -0,0 +1,92 @@
+import 'package:collection/collection.dart';
+import 'package:stowage_plan/models/stowage/slot.dart';
+import 'package:stowage_plan/models/stowage/stowage_plan.dart';
+///
+/// Provides an extension method for pretty-printing a [StowagePlan].
+extension PrettyPrint on StowagePlan {
+  static const String _nullSlot = '    ';
+  static const String _occupiedSlot = '[■] ';
+  static const String _emptySlot = '[ ] ';
+  static const String _rowNumbersPad = '   ';
+  ///
+  /// Prints a textual representation of the stowage plan
+  /// for all bays.
+  void printAll() {
+    for (int bay in _bayIterator()) {
+      printBay(bay);
+    }
+  }
+  ///
+  /// Prints a textual representation of the stowage plan
+  /// for a specific [bay] to the console.
+  void printBay(int bay) {
+    final slotsInBay = toFilteredSlotList(bay: bay);
+    if (slotsInBay.isEmpty) {
+      print('No slots found for bay $bay');
+      return;
+    }
+    print('Bay $bay:');
+    final maxRow = slotsInBay.map((slot) => slot.row).reduce(
+          (a, b) => a > b ? a : b,
+        );
+    final withZeroRow = slotsInBay.any((slot) => slot.row == 0);
+    final maxTier = slotsInBay.map((slot) => slot.tier).reduce(
+          (a, b) => a > b ? a : b,
+        );
+    for (int tier in _tierIterator(maxTier)) {
+      String line = '${tier.toString().padLeft(2, '0')} ';
+      for (int row in _rowIterator(maxRow, withZeroRow)) {
+        final slot = slotsInBay.firstWhereOrNull(
+          (s) => s.row == row && s.tier == tier,
+        );
+        switch (slot) {
+          case null:
+            line += _nullSlot;
+            break;
+          case Slot(containerId: final int _):
+            line += _occupiedSlot;
+            break;
+          case Slot(containerId: null):
+            line += _emptySlot;
+            break;
+        }
+      }
+      print(line);
+    }
+    String rowNumbers = _rowNumbersPad;
+    for (int row in _rowIterator(maxRow, withZeroRow)) {
+      rowNumbers += ' ${row.toString().padLeft(2, '0')} ';
+    }
+    print(rowNumbers);
+  }
+  ///
+  /// Returns [Iterable] collection of unique bay numbers
+  /// present in the stowage plan, sorted in descending order.
+  Iterable<int> _bayIterator() {
+    final uniqueBays = toFilteredSlotList().map((slot) => slot.bay).toSet();
+    final sortedBays = uniqueBays.toList()..sort((a, b) => b.compareTo(a));
+    return sortedBays;
+  }
+  ///
+  /// Returns [Iterable] collection of row numbers
+  /// in accordance with stowage numbering system for rows
+  /// [ISO 9711-1, 3.2](https://www.iso.org/ru/standard/17568.html)
+  Iterable<int> _rowIterator(int maxRow, bool withZeroRow) sync* {
+    for (int row = maxRow; row >= 2; row -= 2) {
+      yield row;
+    }
+    yield withZeroRow ? 0 : 1;
+    for (int row = withZeroRow ? 1 : 3; row <= maxRow; row += 2) {
+      yield row;
+    }
+  }
+  ///
+  /// Returns [Iterable] collection of tier numbers
+  /// in accordance with stowage numbering system for rows
+  /// [ISO 9711-1, 3.3](https://www.iso.org/ru/standard/17568.html)
+  Iterable<int> _tierIterator(int maxTier) sync* {
+    for (int tier = maxTier; tier >= 2; tier -= 2) {
+      yield tier;
+    }
+  }
+}
