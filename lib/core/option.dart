4872575c1279e07d4255657528bfc3522432d5ec
nyaneet <nyaneet@gmail.com>
2024-10-22 01:13:34 +0300

    feat(Result): replace extensions for Result with new ones

---
+++ lib/core/option.dart
@@ -0,0 +1,18 @@
+///
+/// Holds either value or nothing.
+sealed class Option<T> {}
+///
+/// [Option] that holds value.
+final class Some<T> implements Option<T> {
+  final T value;
+  ///
+  /// Create [Option] object that holds [value].
+  const Some(this.value);
+}
+///
+/// [Option] that holds nothing.
+final class None implements Option<Never> {
+  ///
+  /// Create [Option] object that holds nothing.
+  const None();
+}
